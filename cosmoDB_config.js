9273d2c351914af7364b71aa49657f90385863ac
briceln <brice.lang-nguyen@epitech.eu>
2020-06-11 15:06:52 +0200

    :green_heart: Put credentials in code

--- cosmoDB_config.js
+++ cosmoDB_config.js
@@ -1,9 +1,9 @@
 const config = {
-	endpoint: process.env.COSMODB_ENDPOINT,
-	key: process.env.COSMODB_KEY,
-	databaseId: "KumbayaSensing",						// Replace with the name of the database
-	containerId: "SoilMeasurement",						// Replace with the name of the collection in the database
-	partitionKey: {kind: "Hash", paths: ["/userid"]}	// Replace with the partitionKey of the container
+	endpoint: "https://malawi-sensing.documents.azure.com:443/", // URI
+	key: "yBWJ4Nmp8e6WpfY7jFrgdGQ1DdbonOKebQ5M6M1bvpT44tKprKCTZqvbBW2GpNvbru2wKn3vYOxZBbFgBXw2ZQ==",
+	databaseId: "KumbayaSensing",
+	containerId: "SoilMeasurement",
+	partitionKey: {kind: "Hash", paths: ["/userid"]}
 };
 
 module.exports = config;
